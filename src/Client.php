d11b973f8e89072dfa5e85a2400f9c940cef7be5
Dzinekk <tjinek@gmail.com>
2024-11-07 09:42:35 +0100

    fix: guzzle exception

--- src/Client.php
+++ src/Client.php
@@ -2,16 +2,17 @@
 declare(strict_types=1);
 
 namespace Dzinekk\ClaidAI;
-use Exception;
+
+use GuzzleHttp\Exception\GuzzleException;
 
 class Client {
-    protected GuzzleHttp\Client $client;
+    protected \GuzzleHttp\Client $client;
 
     public function __construct(
         private readonly string $apiKey,
         private readonly array $options = [],
     ) {
-        $this->client = new GuzzleHttp\Client([
+        $this->client = new \GuzzleHttp\Client([
             'base_uri' => 'https://api.claid.ai/v1-beta1/',
             'headers' => [
                 'Host' => 'api.claid.ai',
@@ -20,10 +21,10 @@ class Client {
             ]
         ] + $this->options);
     }
-
+    
     /**
-     * @param string                    $method
-     * @param string                    $uri
+     * @param string $method
+     * @param string $uri
      * @param array<string, mixed>|null $data
      * @param array<string, mixed>|null $query
      * @return array<string, mixed>
@@ -47,8 +48,13 @@ class Client {
         if (!empty($data)) {
             $options['json'] = $data;
         }
-
-        $response = $this->client->request($method, $uri, $options);
+        
+        try {
+            $response = $this->client->request($method, $uri, $options);
+        } catch (GuzzleException $e) {
+            throw new ClaidException('Failed making request: '.$e->getMessage());
+        }
+        
         if ($response->getBody()->getSize() === 0) {
             return [];
         }
